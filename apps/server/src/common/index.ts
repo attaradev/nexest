de3cbf288acab201a1de0ee58e9bcf3e5c05158a
Mike Attara <mpyebattara@gmail.com>
2024-03-08 00:29:22 +0000

    feat(server): add prisma

---
+++ apps/server/src/common/index.ts
@@ -0,0 +1,2 @@
+export { PrismaService } from './infra/prisma/prisma.service';
+export { CommonModule } from './common.module';
---
+++ apps/server/src/common/infra/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      this.logger.log('Prisma connected');
+    } catch (error) {
+      this.logger.error(error);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
