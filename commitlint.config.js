a8181908a896fd27ad55fce9981ab2ce60779f0f
Mike Attara <mpyebattara@gmail.com>
2024-03-08 05:16:40 +0000

    🔨 refactor: test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,46 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "revert",
+      ],
+    ],
+    "scope-enum": [2, "always", ["web", "server"]],
+  },
+  plugins: [
+    {
+      rules: {
+        "scope-enum": () => [2, "always", ["web", "server"]],
+        "scope-empty": [2, "never"],
+        "type-enum": ({ subject }) => {
+          console.log(subject);
+          return [
+            2,
+            "always",
+            [
+              "feat",
+              "fix",
+              "chore",
+              "docs",
+              "style",
+              "refactor",
+              "perf",
+              "test",
+              "revert",
+            ],
+          ];
+        },
+      },
+    },
+  ],
 };
